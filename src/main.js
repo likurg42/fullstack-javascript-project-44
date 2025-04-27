d92a349687174e444c361e6120e3b1fdd0ea4372
likurg42 <likurg42@gmail.com>
2025-04-27 23:35:17 +0300

    feat: add welcome text

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import readlineSync from "readline-sync";
+
 export function main() {
-  console.log('main');
+  console.log("Welcome to brain Games!");
+  const name = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${name}!`);
 }
