d05b895f4151c83d4c3806cd55fd44f3fa306214
likurg42 <likurg42@gmail.com>
2025-04-28 00:17:06 +0300

    build: add eslint config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync'
 
 export function main() {
-  console.log("Welcome to brain Games!");
-  const name = readlineSync.question("May I have your name? ");
-  console.log(`Hello, ${name}!`);
+  console.log('Welcome to brain Games!')
+  const name = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${name}!`)
 }
